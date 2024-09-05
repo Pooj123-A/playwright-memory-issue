e6a87b013bdd178fb204759457e31e39fb2b5a59
Km Pooja <kpooja@atlassian.com>
2024-09-05 08:48:41 +0530

    check for increase in memory with version 1.42

--- src/module1422/test.spec.tsx
+++
@@ -1,8 +0,0 @@
-
-import { test, expect } from "@playwright/experimental-ct-react";
-import App from "./App.tsx";
-
-test("should work", async ({ mount }) => {
-  const component = await mount(<App />);
-  await expect(component).toContainText("Hello World");
-});
